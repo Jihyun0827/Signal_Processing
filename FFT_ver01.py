114c044ddf37447cce83a031da39e8eb7b763b7a
Jihyun0827 <jjhhelen90@gmail.com>
2021-01-11 18:00:59 +0900

    Load Data & FFT

--- FFT_ver01.py
+++ FFT_ver01.py
@@ -5,10 +5,11 @@ from matplotlib import pyplot as plt
 
 Defect_Amplitude=[]
 Defect_Time=[]
+i=1
 
 for d in [1]:
     for w in [1, 2, 3]:
-        # plt.subplot(6, 3, i)
+        plt.subplot(3, 1, i)
         ROOT = 'C:/Projects/LSAW_Defect__/Data/Width%d/Depth%d' % (w, d)
         data_file_list = os.listdir(ROOT)
 
@@ -24,30 +25,54 @@ for d in [1]:
         Defect_Amplitude.append(Amplitude_temp)
         Defect_Time.append(Time_temp)
 
+        plt.plot(Time_temp, Amplitude_temp, label='W%d_D%d' % (w, d))
+        plt.ylim((-0.003, 0.003))
+        plt.grid()
+        plt.legend()
+        i += 1
+        
+        
+plt.show()
+
 Defect_Amplitude=np.array(Defect_Amplitude)
 Defect_Time=np.array(Defect_Time)
 
-FFT_Amplitude = Defect_Amplitude[1,:]
-FFT_Time = Defect_Time[1,:]
+Amplitude = Defect_Amplitude[2,:]
+Time = Defect_Time[2,:]
 
+# Cut Time
+Cut_Start_percent=0.02
+Cut_End_percent=0.7
+
+Cut_Start_Idx=round(Cut_Start_percent*len(Amplitude))
+Cut_End_Idx=round((1-Cut_End_percent)*len(Amplitude))
+
+Cut_Amplitude = Amplitude[Cut_Start_Idx:Cut_End_Idx]
+Cut_Time= Time[Cut_Start_Idx:Cut_End_Idx]
+
+plt.plot(Cut_Time,Cut_Amplitude, label='W3_D1')
+plt.ylim((-0.002, 0.002))
+plt.grid()
+plt.legend()
+plt.show()
+
+# FFT
+FFT_Time=Cut_Time
+FFT_Amplitude=Cut_Amplitude
 Sampling_time=FFT_Time[2]-FFT_Time[1]
 
-Sample_Rate = 100  # Hertz
+Sample_Frequency = 100
+Frequency_Range = 5e06 # Hertz
 Sampling_Frequency=1/Sampling_time
 
 FFT_Magnitude=fft(FFT_Amplitude)
 FFT_Frequency=fftfreq(len(FFT_Amplitude)*Sample_Rate, 1/Sample_Rate)
+FFT_Frequency=FFT_Frequency[:Sample_Rate]
 
 plt.plot(FFT_Frequency, FFT_Magnitude)
 plt.show()
 
-2.0 / N * np.abs(yf[0:N // 2])
-
-yf = fft(y)
-xf = np.linspace(0.0, 1.0/(2.0*T), int(N/2))
-
-fig, ax = plt.subplots()
-ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
+plt.plot(FFT_Magnitude)
 plt.show()
 
 # https://docs.scipy.org/doc/scipy/reference/tutorial/fft.html
