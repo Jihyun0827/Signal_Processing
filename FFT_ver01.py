29231130cf62864854b110a34c1bb61bee536f43
Jihyun0827 <jjhhelen90@gmail.com>
2021-01-13 16:36:23 +0900

    FFT

--- FFT_ver01.py
+++ FFT_ver01.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from scipy.fft import fft, fftfreq
 from matplotlib import pyplot as plt
+from scipy.signal import blackman
 
 Defect_Amplitude=[]
 Defect_Time=[]
@@ -9,7 +10,7 @@ i=1
 
 for d in [1]:
     for w in [1, 2, 3]:
-        plt.subplot(3, 1, i)
+
         ROOT = 'C:/Projects/LSAW_Defect__/Data/Width%d/Depth%d' % (w, d)
         data_file_list = os.listdir(ROOT)
 
@@ -25,20 +26,17 @@ for d in [1]:
         Defect_Amplitude.append(Amplitude_temp)
         Defect_Time.append(Time_temp)
 
-        plt.plot(Time_temp, Amplitude_temp, label='W%d_D%d' % (w, d))
-        plt.ylim((-0.003, 0.003))
-        plt.grid()
-        plt.legend()
-        i += 1
-        
-        
-plt.show()
-
 Defect_Amplitude=np.array(Defect_Amplitude)
 Defect_Time=np.array(Defect_Time)
 
 Amplitude = Defect_Amplitude[2,:]
+Amplitude -= np.mean(Amplitude)
 Time = Defect_Time[2,:]
+
+plt.clf()
+plt.subplot(211)
+plt.cla()
+plt.plot(Time*1e06, Amplitude*1e03, label='W%d_D%d' % (w, d))
 
 # Cut Time
 Cut_Start_percent=0.02
@@ -50,65 +48,41 @@ Cut_End_Idx=round((1-Cut_End_percent)*len(Amplitude))
 Cut_Amplitude = Amplitude[Cut_Start_Idx:Cut_End_Idx]
 Cut_Time= Time[Cut_Start_Idx:Cut_End_Idx]
 
-plt.plot(Cut_Time,Cut_Amplitude, label='W3_D1')
-plt.ylim((-0.002, 0.002))
+plt.plot(Cut_Time*1e06,Cut_Amplitude*1e03, label='Cut for FFT')
+plt.xlim((min(Time)*1e06, max(Time)*1e06))
+plt.ylim((min(Cut_Amplitude)*1.2e03, max(Cut_Amplitude)*1.2e03))
+
+plt.xlabel('Time [us]')
+plt.ylabel('Amplitude [mV]')
 plt.grid()
-plt.legend()
-plt.show()
+plt.legend(loc='lower right')
+plt.tight_layout()
 
 # FFT
 FFT_Time=Cut_Time
 FFT_Amplitude=Cut_Amplitude
 Sampling_time=FFT_Time[2]-FFT_Time[1]
 
-Sample_Frequency = 100
-Frequency_Range = 5e06 # Hertz
+Sampling_rate = 100
+Frequency_Range = 3e06 # Hertz
 Sampling_Frequency=1/Sampling_time
+Sampling_Number = len(FFT_Amplitude) * Sampling_rate
 
-FFT_Magnitude=fft(FFT_Amplitude)
-FFT_Frequency=fftfreq(len(FFT_Amplitude)*Sample_Rate, 1/Sample_Rate)
-FFT_Frequency=FFT_Frequency[:Sample_Rate]
+Frequency_Range_parameter=round(Frequency_Range / Sampling_Frequency * Sampling_Number)
+FFT_Frequency=np.linspace(0.0, Frequency_Range_parameter, Frequency_Range_parameter+1)
+FFT_Frequency*=Sampling_Frequency/Sampling_Number
+FFT_Magnitude=fft(FFT_Amplitude,Sampling_Number)
+FFT_Magnitude=np.abs(FFT_Magnitude)/len(FFT_Amplitude)*2
+FFT_Magnitude = FFT_Magnitude[0:len(FFT_Frequency)]
 
-plt.plot(FFT_Frequency, FFT_Magnitude)
-plt.show()
 
-plt.plot(FFT_Magnitude)
-plt.show()
-
-# https://docs.scipy.org/doc/scipy/reference/tutorial/fft.html
-
-from scipy.fft import fft, fftfreq
-# Number of sample points
-N = 600
-# sample spacing
-T = 1.0 / 800.0
-x = np.linspace(0.0, N*T, N, endpoint=False)
-y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
-
-plt.plot(x,y)
-plt.show()
-yf = fft(y)
-xf = fftfreq(N, T)[:N//2]
-import matplotlib.pyplot as plt
-plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+plt.subplot(212)
+plt.cla()
+plt.plot(FFT_Frequency/1e06, FFT_Magnitude*1e03)
+plt.xlabel('Frequency [MHz]')
+plt.ylabel('Magnitude [mV]')
+plt.xlim((min(FFT_Frequency)/1e06, max(FFT_Frequency)/1e06))
+plt.ylim((0, max(FFT_Magnitude)*1.2e03))
 plt.grid()
-plt.show()
-
-from scipy.fft import fft, fftfreq
-# Number of sample points
-N = 600
-# sample spacing
-T = 1.0 / 800.0
-x = np.linspace(0.0, N*T, N, endpoint=False)
-y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
-yf = fft(y)
-from scipy.signal import blackman
-w = blackman(N)
-ywf = fft(y*w)
-xf = fftfreq(N, T)[:N//2]
-import matplotlib.pyplot as plt
-plt.semilogy(xf[1:N//2], 2.0/N * np.abs(yf[1:N//2]), '-b')
-plt.semilogy(xf[1:N//2], 2.0/N * np.abs(ywf[1:N//2]), '-r')
-plt.legend(['FFT', 'FFT w. window'])
-plt.grid()
+plt.tight_layout()
 plt.show()
